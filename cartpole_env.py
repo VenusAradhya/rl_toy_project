3fe8bdc74f25f860e0bdb1bfdf808f31972384f6
Venus <72524551+VenusAradhya@users.noreply.github.com>
2026-02-11 13:22:48 -0800

    Refactor episode loop and add progress logging
    Refactored the episode loop to improve structure and added a print statement for every 100 episodes.

--- cartpole_env.py
+++ cartpole_env.py
@@ -1,19 +1,27 @@
 import gymnasium as gym
 
-env = gym.make("CartPole-v1", render_mode="human")
-state, _ = env.reset()
+#env = gym.make("CartPole-v1", render_mode="human")
+env = gym.make("CartPole-v1", render_mode="human") #wo graphics
 
-done = False
-total_reward = 0
+for episode in range(3000):
+    state, _ = env.reset()
+    done = False
+    total_reward = 0
 
-while not done:
-    env.render()
-    action = env.action_space.sample()  #random action
-    state, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
-    total_reward += reward
+    while not done:
+        env.render()
+        action = env.action_space.sample()  #random action
+        state, reward, terminated, truncated, info = env.step(action)
+
+        total_reward += reward
+        done = terminated or truncated
+
+    if episode %100 == 0:
+        print (f"Epsiode {epsiode}: Total = {total_reward}")
 
 print("Total reward:", total_reward)
 env.close()
+
+
 
 
